af3dae395e3804061512617427864dfb0da98091
Pradeep G <itsprdp@gmail.com>
2013-10-29 15:57:57 +0530

    update the knife config

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,7 +1,10 @@
-cookbook_path ["cookbooks", "site-cookbooks"]
-node_path     "nodes"
-role_path     "roles"
-data_bag_path "data_bags"
-#encrypted_data_bag_secret "data_bag_key"
+log_level                :info
+log_location             STDOUT
+node_name                'admin'
+client_key               '/home/itsprdp/chef-server-example/.chef/admin.pem'
+validation_client_name   'chef-validator'
+validation_key           '/etc/chef-server/chef-validator.pem'
+chef_server_url          'https://chef.itsprdp.com'
+syntax_check_cache_path  '/home/itsprdp/chef-server-example/.chef/syntax_check_cache'
+cookbook_path            [ './cookbooks', './site-cookbooks' ]
 
-knife[:berkshelf_path] = "cookbooks"
